72be97ff7bd0672201409eb907804d0ecf8de65b
aitalyy <aitalyy@gmail.com>
2020-02-20 15:08:06 +0900

    add models .cs

---
+++ KinoSoft/KinoSoft/models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace KinoSoft
+{
+    class models
+    {
+        public class Movie
+        {
+            [Key]
+            public int Id { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
