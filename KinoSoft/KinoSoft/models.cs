635341e1feaab5082d61918fc08a651fa0e810df
aitalyy <aitalyy@gmail.com>
2020-02-20 15:33:48 +0900

    added class Actor,Producer and Report

--- KinoSoft/KinoSoft/models.cs
+++ KinoSoft/KinoSoft/models.cs
@@ -29,6 +29,7 @@ namespace KinoSoft
             [Key]
             public int Id { get; set; }
             public string Name { get; set; }
+            public ICollection<Movie> Movie;
         }
 
         public class Passport
@@ -40,6 +41,28 @@ namespace KinoSoft
             public string LastName { get; set; }
             public string SecondName { get; set; }
             
+        }
+        //Actor, Producer, Report
+        public class Actor
+        {
+            [Key]
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string SecondName { get; set; }
+        }
+        public class Producer
+        {
+            [Key]
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string SecondName { get; set; }
+        }
+        public class Report
+        {
+            [Key]
+            public int Id { get; set; }
         }
     }
 }
