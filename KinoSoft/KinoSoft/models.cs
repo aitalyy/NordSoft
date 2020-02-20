556838ababdc4e56ff3508d88b7b59d3c763bdb7
NEFUstudent <study.svfu@gmail.com>
2020-02-20 15:22:11 +0900

    Added class Passport

--- KinoSoft/KinoSoft/models.cs
+++ KinoSoft/KinoSoft/models.cs
@@ -16,5 +16,16 @@ namespace KinoSoft
             public int Id { get; set; }
             public string name { get; set; }
         }
+
+        public class Passport
+        {
+            [Key]
+            public int number { get; set; }
+            public int series { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string SecondName { get; set; }
+            
+        }
     }
 }
