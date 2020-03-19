e8c2503d92ba509db36781dc4a9e6ff12da0c2f3
k4nob1 <45601148+k4nob1@users.noreply.github.com>
2020-03-19 13:49:29 +0900

    Added perehod to AddMovie

--- KinoSoft/KinoSoft/Form1.Designer.cs
+++ KinoSoft/KinoSoft/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace KinoSoft
+namespace KinoSoft
 {
     partial class Form1
     {
@@ -39,9 +39,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 36);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(151, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Добавление фильма";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
