14ec41384b6c59b19ed31312843d41c1cf9cb140
k4nob1 <45601148+k4nob1@users.noreply.github.com>
2020-03-19 13:42:14 +0900

    Update AddMovie Form

--- KinoSoft/KinoSoft/Forms/AddMovie.Designer.cs
+++ KinoSoft/KinoSoft/Forms/AddMovie.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace KinoSoft.Forms
+namespace KinoSoft.Forms
 {
     partial class AddMovie
     {
@@ -29,183 +29,230 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MovieName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Data = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Actors = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Country = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Producer = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Opisanie = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.Genre = new System.Windows.Forms.CheckedListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Category = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 29);
+            this.label1.Location = new System.Drawing.Point(28, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Название фильма";
             // 
-            // textBox1
+            // MovieName
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 23);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(321, 25);
-            this.textBox1.TabIndex = 1;
+            this.MovieName.Location = new System.Drawing.Point(131, 12);
+            this.MovieName.Multiline = true;
+            this.MovieName.Name = "MovieName";
+            this.MovieName.Size = new System.Drawing.Size(321, 25);
+            this.MovieName.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 448);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 38);
+            this.button1.Size = new System.Drawing.Size(117, 38);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Назад";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(298, 448);
+            this.button2.Location = new System.Drawing.Point(335, 448);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 38);
+            this.button2.Size = new System.Drawing.Size(117, 38);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Добавить";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox2
+            // Data
             // 
-            this.textBox2.Location = new System.Drawing.Point(105, 54);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(321, 25);
-            this.textBox2.TabIndex = 5;
+            this.Data.Location = new System.Drawing.Point(131, 119);
+            this.Data.Multiline = true;
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(321, 25);
+            this.Data.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 60);
+            this.label2.Location = new System.Drawing.Point(90, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
             // 
-            // textBox5
+            // Actors
             // 
-            this.textBox5.Location = new System.Drawing.Point(105, 85);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(321, 25);
-            this.textBox5.TabIndex = 13;
+            this.Actors.Location = new System.Drawing.Point(131, 248);
+            this.Actors.Multiline = true;
+            this.Actors.Name = "Actors";
+            this.Actors.Size = new System.Drawing.Size(321, 25);
+            this.Actors.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(64, 91);
+            this.label5.Location = new System.Drawing.Point(92, 51);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(36, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "label5";
+            this.label5.Text = "Жанр";
             // 
-            // textBox3
+            // Country
             // 
-            this.textBox3.Location = new System.Drawing.Point(105, 116);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(321, 25);
-            this.textBox3.TabIndex = 15;
+            this.Country.Location = new System.Drawing.Point(131, 186);
+            this.Country.Multiline = true;
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(321, 25);
+            this.Country.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 122);
+            this.label3.Location = new System.Drawing.Point(68, 282);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 14;
-            this.label3.Text = "label3";
+            this.label3.Text = "Описание";
             // 
-            // textBox4
+            // Producer
             // 
-            this.textBox4.Location = new System.Drawing.Point(105, 147);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(321, 25);
-            this.textBox4.TabIndex = 17;
+            this.Producer.Location = new System.Drawing.Point(131, 217);
+            this.Producer.Multiline = true;
+            this.Producer.Name = "Producer";
+            this.Producer.Size = new System.Drawing.Size(321, 25);
+            this.Producer.TabIndex = 17;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(64, 153);
+            this.label4.Location = new System.Drawing.Point(57, 124);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 16;
-            this.label4.Text = "label4";
+            this.label4.Text = "Год выпуска";
             // 
-            // textBox6
+            // Opisanie
             // 
-            this.textBox6.Location = new System.Drawing.Point(105, 178);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(321, 25);
-            this.textBox6.TabIndex = 19;
+            this.Opisanie.Location = new System.Drawing.Point(131, 279);
+            this.Opisanie.Multiline = true;
+            this.Opisanie.Name = "Opisanie";
+            this.Opisanie.Size = new System.Drawing.Size(321, 155);
+            this.Opisanie.TabIndex = 19;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(64, 184);
+            this.label6.Location = new System.Drawing.Point(6, 157);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(122, 13);
             this.label6.TabIndex = 18;
-            this.label6.Text = "label6";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(105, 209);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(321, 25);
-            this.textBox7.TabIndex = 21;
+            this.label6.Text = "Возрастная категория";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(64, 215);
+            this.label7.Location = new System.Drawing.Point(86, 191);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(43, 13);
             this.label7.TabIndex = 20;
-            this.label7.Text = "label7";
+            this.label7.Text = "Страна";
+            // 
+            // Genre
+            // 
+            this.Genre.FormattingEnabled = true;
+            this.Genre.Items.AddRange(new object[] {
+            "Комедия",
+            "Драма",
+            "Хоррор",
+            "Триллер",
+            "Боевик"});
+            this.Genre.Location = new System.Drawing.Point(131, 49);
+            this.Genre.Name = "Genre";
+            this.Genre.Size = new System.Drawing.Size(321, 64);
+            this.Genre.TabIndex = 22;
+            this.Genre.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Актёрский состав";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(71, 222);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Режиссер";
+            // 
+            // Category
+            // 
+            this.Category.FormattingEnabled = true;
+            this.Category.Items.AddRange(new object[] {
+            "0+",
+            "6+",
+            "12+",
+            "16+",
+            "18+",
+            "21+"});
+            this.Category.Location = new System.Drawing.Point(131, 154);
+            this.Category.Name = "Category";
+            this.Category.Size = new System.Drawing.Size(321, 21);
+            this.Category.TabIndex = 26;
             // 
             // AddMovie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 498);
-            this.Controls.Add(this.textBox7);
+            this.ClientSize = new System.Drawing.Size(464, 498);
+            this.Controls.Add(this.Category);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Genre);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Opisanie);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Producer);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Country);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Actors);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Data);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.MovieName);
             this.Controls.Add(this.label1);
             this.Name = "AddMovie";
-            this.Text = "AddMovie";
+            this.Text = "Добавление фильма";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,20 +261,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox MovieName;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Data;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox Actors;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox Country;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox Producer;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox Opisanie;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckedListBox Genre;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox Category;
     }
 }
