b3ce9f562b7796aa83e3b59b2e87dd3f65c3c424
k4nob1 <45601148+k4nob1@users.noreply.github.com>
2020-03-19 13:44:34 +0900

    Added new items

--- KinoSoft/KinoSoft/Forms/AddMovie.cs
+++ KinoSoft/KinoSoft/Forms/AddMovie.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,40 @@ namespace KinoSoft.Forms
         public AddMovie()
         {
             InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                    "Если вы закроете, то введённые данные будут сброшены. Вы уверены в этом?",
+                    "Сообщение",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.ServiceNotification);
+            if (result == DialogResult.Yes) Close();
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string genrelist="";
+            foreach (var item in Genre.CheckedItems)
+            {
+                genrelist += item.ToString() + ";";
+            }
+            using (Contex db = new Contex())
+            {
+                Movie movie = new Movie
+                {
+                    Name = MovieName.Text,
+                };
+            }
         }
     }
 }
