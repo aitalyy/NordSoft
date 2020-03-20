b382e297a21fc93ecfdd4b720bc1684f6eefa226
Aital <aitalyy@gmail.com>
2020-03-20 15:58:01 +0900

    edit config

--- KinoSoft/KinoSoft/Migrations/202003200657039_First.Designer.cs
+++ KinoSoft/KinoSoft/Migrations/202003200657039_First.Designer.cs
@@ -13,7 +13,7 @@ namespace KinoSoft.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202003200500029_First"; }
+            get { return "202003200657039_First"; }
         }
         
         string IMigrationMetadata.Source
---
+++ KinoSoft/KinoSoft/Migrations/202003200657039_First.cs
@@ -0,0 +1,34 @@
+﻿namespace KinoSoft.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class First : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Role",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Person", "Password", c => c.String());
+            AddColumn("dbo.Person", "RoleId", c => c.Int());
+            CreateIndex("dbo.Person", "RoleId");
+            AddForeignKey("dbo.Person", "RoleId", "dbo.Role", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Person", "RoleId", "dbo.Role");
+            DropIndex("dbo.Person", new[] { "RoleId" });
+            DropColumn("dbo.Person", "RoleId");
+            DropColumn("dbo.Person", "Password");
+            DropTable("dbo.Role");
+        }
+    }
+}
