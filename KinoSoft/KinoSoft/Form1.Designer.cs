666b48f09f0af704504ee49048e16415829a2b0e
k4nob1 <vinnok991@gmail.com>
2020-04-09 17:33:53 +0900

    merge

--- KinoSoft/KinoSoft/Form1.Designer.cs
+++ KinoSoft/KinoSoft/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace KinoSoft
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Timer timer1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -230,6 +231,7 @@ namespace KinoSoft
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
