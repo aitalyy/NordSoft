7ed72d52b736bf967d2e19d726c0f188549dc77b
fiitt <vvnfiit@gmail.com>
2020-04-09 18:07:53 +0900

    modified SearchForm

--- KinoSoft/KinoSoft/SearchForm.Designer.cs
+++ KinoSoft/KinoSoft/SearchForm.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.searchText = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.dataAll = new System.Windows.Forms.DataGridView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.addText = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataAll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchText
+            // 
+            this.searchText.Location = new System.Drawing.Point(12, 12);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(300, 20);
+            this.searchText.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.searchButton.Location = new System.Drawing.Point(318, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 20);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Поиск";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // dataAll
+            // 
+            this.dataAll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataAll.Location = new System.Drawing.Point(12, 38);
+            this.dataAll.Name = "dataAll";
+            this.dataAll.Size = new System.Drawing.Size(381, 373);
+            this.dataAll.TabIndex = 13;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(318, 418);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 20);
+            this.addButton.TabIndex = 15;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // addText
+            // 
+            this.addText.Location = new System.Drawing.Point(12, 418);
+            this.addText.Name = "addText";
+            this.addText.Size = new System.Drawing.Size(300, 20);
+            this.addText.TabIndex = 14;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(12, 444);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 20);
+            this.cancelButton.TabIndex = 16;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(318, 444);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 20);
+            this.saveButton.TabIndex = 17;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(409, 471);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.addText);
+            this.Controls.Add(this.dataAll);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchText);
+            this.Name = "SearchForm";
             this.Text = "SearchForm";
+            this.Load += new System.EventHandler(this.SearchForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataAll)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox searchText;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView dataAll;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox addText;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveButton;
     }
 }
--- KinoSoft/KinoSoft/SearchForm.cs
+++ KinoSoft/KinoSoft/SearchForm.cs
@@ -12,9 +12,50 @@ namespace KinoSoft
 {
     public partial class SearchForm : Form
     {
-        public SearchForm()
+        private Contex db = new Contex();
+        private Tables table = Tables.Actor;
+
+        public SearchForm(Tables table)
         {
             InitializeComponent();
+        }
+
+        public void UpdateTable()
+        {
+            switch (table)
+            {
+                case Tables.Actor:
+                    dataAll.DataSource = db.Actors.ToList<Actor>();
+                    break;
+                case Tables.Producer:
+                    dataAll.DataSource = db.Producers.ToList<Producer>();
+                    break;
+                case Tables.Country:
+                    dataAll.DataSource = db.Countrys.ToList<Country>();
+                    break;
+                case Tables.Genre:
+                    dataAll.DataSource = db.Genres.ToList<Genre>();
+                    break;
+                default:
+                    DialogResult result = MessageBox.Show(
+                    "В процессе разработки!",
+                    "Сообщение",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.ServiceNotification);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
