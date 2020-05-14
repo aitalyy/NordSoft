02f488712451db241c3e7e7027ae8ebe47fa2163
fiitt <vvnfiit@gmail.com>
2020-05-15 05:23:35 +0900

    added ReportForm

---
+++ KinoSoft/KinoSoft/ReportForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace KinoSoft
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.typeReport = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // reportButton
+            // 
+            this.reportButton.Location = new System.Drawing.Point(350, 265);
+            this.reportButton.Name = "reportButton";
+            this.reportButton.Size = new System.Drawing.Size(75, 23);
+            this.reportButton.TabIndex = 0;
+            this.reportButton.Text = "Отчет";
+            this.reportButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 265);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // typeReport
+            // 
+            this.typeReport.FormattingEnabled = true;
+            this.typeReport.Location = new System.Drawing.Point(12, 12);
+            this.typeReport.Name = "typeReport";
+            this.typeReport.Size = new System.Drawing.Size(413, 21);
+            this.typeReport.TabIndex = 2;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 300);
+            this.Controls.Add(this.typeReport);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.reportButton);
+            this.Name = "ReportForm";
+            this.Text = "ReportForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reportButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ComboBox typeReport;
+    }
+}
