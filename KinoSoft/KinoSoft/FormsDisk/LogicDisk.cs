b913a9c69b85720aafd2a04bf27494b2db6084e8
Aital <aitalyy@gmail.com>
2020-05-14 23:27:12 +0900

    q

--- KinoSoft/KinoSoft/FormsDisk/LogicDisk.cs
+++ KinoSoft/KinoSoft/FormsDisk/LogicDisk.cs
@@ -33,47 +33,23 @@ namespace KinoSoft
                 };
 
                 db.Disks.Add(disk);
-
-                //MovieCategory movieCategory = new MovieCategory
-                //{
-                //    Category = "18+",
-                //};
-                //db.MovieCategory.Add(movieCategory);
-
-                //Movie movie10 = db.Movies.Where(k => k.Id == 3).FirstOrDefault();
-                //Movie movie11 = db.Movies.Where(k => k.Id == 5).FirstOrDefault();
-                //Movie movie12 = db.Movies.Where(k => k.Id == 6).FirstOrDefault();
-                //Movie movie13 = db.Movies.Where(k => k.Id == 7).FirstOrDefault();
-                //movie10.Category = movieCategory;
-                //movie11.Category = movieCategory;
-                //movie12.Category = movieCategory;
-                //movie13.Category = movieCategory;
-                //movie10.CategoryId = movieCategory.Id;
-                //movie11.CategoryId = movieCategory.Id;
-                //movie12.CategoryId = movieCategory.Id;
-                //movie13.CategoryId = movieCategory.Id;
-
-                using (Contex My = new Contex())
+                for (int i = 0; i < ArrayMoviesDisk.movies.Count; i++)
                 {
-                    for (int i = 0; i < ArrayMoviesDisk.movies.Count; i++)
+                    //var movie = ArrayMoviesDisk.movies[i];
+                    int movieId = Convert.ToInt32(ArrayMoviesDisk.arrayList[i]);
+                    //int movieId = movie.Id;
+                    Movie movie = db.Movies.Where(k => k.Id == movieId).FirstOrDefault()
+                    int diskId = disk.Id;
+                    MovieDisk movieDisk = new MovieDisk
                     {
-                        //var movie = ArrayMoviesDisk.movies[i];
-                        int movieId = Convert.ToInt32(ArrayMoviesDisk.arrayList[i]);
-                        //int movieId = movie.Id;
-                        Movie movie = My.Movies.Where(k => k.Id == movieId).FirstOrDefault();
-
-                        int diskId = disk.Id;
-                        MovieDisk movieDisk = new MovieDisk
-                        {
-                            MovieId = movieId,
-                            Movie = movie,
-                            DiskId = diskId,
-                            Disk = disk,
-                        };
-                        My.MovieDisks.Add(movieDisk);
-                    }
-                    My.SaveChanges();
+                        MovieId = movieId,
+                        Movie = movie,
+                        DiskId = diskId,
+                        Disk = disk,
+                    };
+                    db.MovieDisks.Add(movieDisk);
                 }
+                db.SaveChanges();
             }
         }
     }
