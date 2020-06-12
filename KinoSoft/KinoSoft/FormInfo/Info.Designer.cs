349596d7323a589dad7025428de65e793ccc0123
k4nob1 <vinnok991@gmail.com>
2020-06-12 15:51:08 +0900

    asd

--- KinoSoft/KinoSoft/FormInfo/Info.Designer.cs
+++ KinoSoft/KinoSoft/FormInfo/Info.Designer.cs
@@ -28,23 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Info));
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(219, 130);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(12, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(219, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Location = new System.Drawing.Point(102, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
             // 
             // Info
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 450);
+            this.ClientSize = new System.Drawing.Size(243, 224);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Info";
             this.Text = "Info";
@@ -56,5 +105,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- KinoSoft/KinoSoft/FormInfo/Info.cs
+++ KinoSoft/KinoSoft/FormInfo/Info.cs
@@ -15,6 +15,56 @@ namespace KinoSoft.FormInfo
         public Info()
         {
             InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        float step = 0;
+        int i = 0;
+        Color currentColor = Color.DarkGreen;
+        Color targetColor = Color.LightBlue;
+        Random rnd = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 1000)
+            {
+                label1.Visible = true;
+                progressBar1.Value = 1000;
+                timer1.Stop();
+            }
+            else
+            {
+                label2.Text = (i/10).ToString() + "%";
+                i++;
+                progressBar1.Value += 1;
+            }
+
+            if (step >= 1f)
+            {
+                step = 0;
+
+                int R = rnd.Next(0, 255);
+                int G = rnd.Next(0, 255);
+                int B = rnd.Next(0, 255);
+                currentColor = targetColor;
+                targetColor = Color.FromArgb(R, G, B);
+            }
+            int mixR = (int)(currentColor.R * (1f - step) + targetColor.R * step);
+            int mixG = (int)(currentColor.G * (1f - step) + targetColor.G * step);
+            int mixB = (int)(currentColor.B * (1f - step) + targetColor.B * step);
+            this.BackColor = Color.FromArgb(mixR, mixG, mixB);
+            this.button1.BackColor = Color.FromArgb(mixR, mixG, mixB);
+            step += 0.01f;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
